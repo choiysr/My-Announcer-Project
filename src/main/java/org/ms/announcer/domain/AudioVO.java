42b827270b8bdc7e82db63620635440615e9e38a
Kim ManKi <aksrl25@gmail.com>
2019-12-11 15:26:46 +0900

    mangit

---
+++ src/main/java/org/ms/announcer/domain/AudioVO.java
@@ -0,0 +1,23 @@
+package org.ms.announcer.domain;
+
+import javax.persistence.Embeddable;
+
+import lombok.Data;
+
+/**
+ * BCBoardDTO
+ */
+
+@Data
+@Embeddable
+public class AudioVO {
+
+    
+    private String audioPath;
+    private String audioName;
+    
+    private String alarmBells;
+    private String intro;
+    private String end;
+    
+}

bbe76e2236c23483eb6ea7c063bca30ba7b07ef2
Kim ManKi <aksrl25@gmail.com>
2019-12-26 17:54:11 +0900

    add_service in Controller

--- src/main/java/org/ms/announcer/controller/CPBoardController.java
+++ src/main/java/org/ms/announcer/controller/CPBoardController.java
@@ -1,8 +1,13 @@
 package org.ms.announcer.controller;
 
+import org.ms.announcer.service.CPBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.Setter;
 
 public class CPBoardController {
 
+    @Setter(onMethod_ = {@Autowired})
+    private CPBoardService cpBoardService;
     
 }

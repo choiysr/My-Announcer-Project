7edf9b63923ad3eb258705da2f7e9beba950c87b
choiysr <valsa_de_euridice@naver.com>
2019-12-26 17:38:37 +0900

    cpside_created

--- src/main/java/org/ms/announcer/controller/BasicController.java
+++ src/main/java/org/ms/announcer/controller/BasicController.java
@@ -1,12 +1,8 @@
 package org.ms.announcer.controller;
 
-import org.ms.announcer.service.BCBoardService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.Setter;
 
 /**
  * BaiscController

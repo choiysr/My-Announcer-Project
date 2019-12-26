f158818f9decf13ed393766c4405b6cc6b742b88
Kim ManKi <aksrl25@gmail.com>
2019-12-26 17:18:48 +0900

    add_save cpboard service

--- src/main/java/org/ms/announcer/service/CPBoardServiceImpl.java
+++ src/main/java/org/ms/announcer/service/CPBoardServiceImpl.java
@@ -1,9 +1,23 @@
 package org.ms.announcer.service;
 
+import org.ms.announcer.domain.CPBoard;
+import org.ms.announcer.repositories.CPBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lombok.Setter;
 
 @Service
 public class CPBoardServiceImpl implements CPBoardService {
+    @Setter(onMethod_ = { @Autowired })
+    private CPBoardRepository cprepo;
 
+    @Override
+    public void cpRegister(CPBoard cpboard) {
+        cprepo.save(cpboard);
+    }
+
+
+    
     
 }

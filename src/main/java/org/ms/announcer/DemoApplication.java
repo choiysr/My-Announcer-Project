b4cc9d31ac4c877c3b34a73157a028d4134628b6
choiysr <valsa_de_euridice@naver.com>
2020-01-16 15:49:08 +0900

    add_scheduling

--- src/main/java/org/ms/announcer/DemoApplication.java
+++ src/main/java/org/ms/announcer/DemoApplication.java
@@ -1,15 +1,28 @@
 package org.ms.announcer;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
 
-		
+	// 매월 오전 8시에 실행 
+	@Scheduled(cron="0 0 08 * * ?")
+	public void tmpFilesDelete() {
+		File file = new File("C:\\AudioStorage\\tmp");
+			File[] files = file.listFiles();
+			for(File eachfile : files) {
+				eachfile.delete();
+			}
 	}
 }
 	
---
+++ src/main/java/org/ms/announcer/service/FileDelete.java
@@ -0,0 +1,15 @@
+package org.ms.announcer.service;
+
+import java.io.File;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * FileDelete
+ */
+public class FileDelete {
+
+  
+
+    
+}
